0cab88e183f74e027d3143ad9d94ed2239d3ebeb
jgchenu <jgchenu@foxmail.com>
2019-07-16 00:11:11 +0800

    feat: html-webpack-plugin

---
+++ demo4/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// npx webpack --config webpack.config11.js 指定配置文件打包
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(jpeg|jpg|png|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[name]_[hash].[ext]',
+            outputPath: 'images/',
+            limit: 204800
+          }
+        }
+      },
+      {
+        test: /\.(eot|ttf|svg)$/,
+        use: {
+          loader: 'file-loader'
+        }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              modules: true
+            }
+          },
+          'postcss-loader',
+          'less-loader'
+        ]
+      }
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin()
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+};

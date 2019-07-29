3197545e850e54634a0515b876cc6820fce0e9f5
jgchenu <jgchenu@foxmail.com>
2019-07-29 23:55:22 +0800

    feat: async loader

--- demo18-make-loader/loaders/replaceLoader.js
+++ demo18-make-loader/loaders/replaceLoader.js
@@ -1,7 +1,16 @@
 const loaderUtils = require('loader-utils');
 module.exports = function(source) {
   const options = loaderUtils.getOptions(this);
-  // return source.replace('dell', options.name);
+  /**
+   * 等价效果  
+   * this.callback(null, result);
+   * return source.replace('dell', options.name);
+   *
+   **/
   const result = source.replace('dell', options.name);
-  this.callback(null, result);
+  // 异步效果
+  const callback = this.async();
+  setTimeout(() => {
+    callback(null,result);
+  }, 2000);
 };
